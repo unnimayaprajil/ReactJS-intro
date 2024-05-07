db895d3dd8e409c0cb494470290c84285f254a64
unnimayaprajil <unnimayasubramanyan@gmail.com>
2024-05-07 11:47:07 +0530

    components added

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const About =()=>{
+    return(
+        <h2>Hello about</h2>
+    )
+}
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Contact =()=>{
+    return(
+        <h2>Hello contact</h2>
+    )
+}
+export default Contact
